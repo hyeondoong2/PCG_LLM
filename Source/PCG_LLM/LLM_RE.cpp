368511ad942532f13582f3d16ad4069ef292169b
hyeondoong <sallybi@naver.com>
2025-12-23 01:42:00 +0900

    update pcg node

--- Source/PCG_LLM/LLM_RE.cpp
+++ Source/PCG_LLM/LLM_RE.cpp
@@ -13,7 +13,7 @@ DEFINE_LOG_CATEGORY(LogLLM);
 ALLM_RE::ALLM_RE()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
 }
 
